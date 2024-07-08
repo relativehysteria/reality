8b9a8e95844737ed54717c9a91c90e063cc6f3e9
relativehysteria <rruziro@gmail.com>
2024-07-08 18:32:00 +0200

    h

--- main.py
+++ main.py
@@ -85,14 +85,7 @@ def get_new_listings(new, old):
 
 
 if __name__ == "__main__":
-    regions = ["okres Brno-město"]
-    dispositions = ["1+kk", "1+1", "2+kk", "2+1", "3+kk", "3+1", "4+kk", "4+1"]
-    filters = [
-        lambda listing: listing.area > 40,
-        lambda listing: listing.price < 20000,
-    ]
-
-    DB_NAME = "ids"
+    DB_NAME = "ids.txt"
 
     # Get new data, load old data and check new IDs
     print("it's free real estate...")
